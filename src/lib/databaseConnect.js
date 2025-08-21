240e74fc33810130482b3e044ef620663647095c
Sauda001 <sauda.cse8.bu@gmail.com>
2025-08-21 23:08:26 +0600

    Add MongoDB integration and update package dependencies

---
+++ src/lib/databaseConnect.js
@@ -0,0 +1,36 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.db_uri;
+
+export const collectionList={
+    usersCollection: "usersCollection",
+    productsCollection: "productsCollection",
+}
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export async function databaseConnect(collectionName){
+try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    return await client.db(process.env.db_name).collection(collectionName);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+
+}
+
+
+
+
